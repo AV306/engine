7c3740396a7038c595578a46a8773b27ee84ebc1
AV306 <brandonjoash@gmail.com>
2024-01-06 22:58:40 +0800

    help

--- map.cpp
+++ map.cpp
@@ -1,16 +1,29 @@
 #pragma once
 
+#include <SFML/Graphics.hpp>
+
 #include "types.hpp"
+
 #include <cstddef>
 #include <exception>
 #include <fstream>
+#include <iostream>
 #include <string>
+#include <utility>
 #include <vector>
 
 using namespace std;
 
 namespace Engine
 {
+    sf::Texture tilemap;
+
+    void initTilemap( string tilemap_path )
+    {
+        if ( !tilemap.loadFromFile( tilemap_path ) )
+            throw invalid_argument{ "Failed to load sprite atlas" };
+    }
+
     struct Map
     {
         Map( string filename )
@@ -20,7 +33,7 @@ namespace Engine
             // Ensure map file is valid
             char M, A, P;
             map_ifstream >> M >> A >> P;
-            if ( M != 'M' || A != 'A' || P != 'P' ) throw invalid_argument{ "Invalid map file provided" };
+            if ( !map_ifstream || M != 'M' || A != 'A' || P != 'P' ) throw invalid_argument{ "Invalid map file provided" };
 
             // Read width and height from map
             map_ifstream >> this->width >> this->height;
@@ -30,8 +43,19 @@ namespace Engine
             this->tiles = new uchar[width*height];
 
             // Populate tile array
-            for ( auto i = 0; i < width*height; i++ )
-                map_ifstream >> this->tiles[i];
+            for ( auto i = 0; i < width*height; i++ ) map_ifstream >> this->tiles[i];
+
+            // Build texture
+            this->build_texture();
+        }
+
+        Map( uchar width, uchar height, uchar* tiles ) : width{ width }, height{ height }
+        {
+            // Populate tile array
+            this->tiles = std::move( tiles );
+
+            // Build texture
+            this->build_texture();
         }
 
         ~Map() noexcept
@@ -40,9 +64,18 @@ namespace Engine
             tiles = nullptr;
         }
 
-        uchar width; // Width of map in tiles
-        uchar height; // Height of map in tiles
-        uchar* tiles; // Array of tilemap indexes
-        //vector<ushort> border_tiles; // Array of tile array indexes that cannot be moved onto
+        private:
+            uchar width; // Width of map in tiles
+            uchar height; // Height of map in tiles
+            uchar* tiles; // Array of tilemap indexes
+
+            sf::Texture texture; // Complete map texture
+
+            //vector<ushort> border_tiles; // Array of tile array indexes that cannot be moved onto
+
+            void build_texture()
+            {
+                this->texture.create( )
+            }
     };
 }
