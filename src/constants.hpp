b560a8d61763da65bff3dbf2a37ef4a8747af311
AV3_08 <brandonjoash@gmail.com>
2024-01-12 02:33:26 +0000

    fix catefiles

--- src/constants.hpp
+++ src/constants.hpp
@@ -1,12 +1,17 @@
 #pragma once
 
+#include <cstdint>
+#include "types.hpp"
 // TODO: make data-driven
 
 // RPG Maker VX Ace uses 32x32 tiles, and each tilemap is 8 tiles by 16 tiles
-#define TILESET_TILE_SIZE_PIXELS 32
-#define TILESET_WIDTH_TILES 8
-#define TILESET_HEIGHT_TILES 16
+// 
+const uint TILESET_TILE_SIZE_PIXELS = 32;
 
-#define SPRITE_ATLAS_SPRITE_SIZE_PIXELS 64
-#define SPRITE_ATLAS_WIDTH_SPRITES 12
-#define SPRITE_ATLAS_HEIGHT_SPRITES 8
+// These two multiplied together must be <= 255
+const uint8_t TILESET_WIDTH_TILES = 8;
+const uint8_t TILESET_HEIGHT_TILES = 16;
+
+const uint SPRITE_ATLAS_SPRITE_SIZE_PIXELS = 64;
+const uint SPRITE_ATLAS_WIDTH_SPRITES = 12;
+const uint SPRITE_ATLAS_HEIGHT_SPRITES = 8;
