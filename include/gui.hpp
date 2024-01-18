585f1d9eb7ef9781ba3215a4a4735e169d882e0d
AV3_08 <brandonjoash@gmail.com>
2024-01-18 02:09:34 +0000

    Implemented basic GUI canvas

--- include/gui.hpp
+++ include/gui.hpp
@@ -2,10 +2,51 @@
 
 #include <SFML/Graphics.hpp>
 
+#include <filesystem>
+
+#include "defines.hpp"
+#include "types.hpp"
+
 namespace Engine
 {
-    struct GuiCanvas
+    struct GuiCanvas extends public sf::Drawable, sf::Transformable
     {
-        GuiCanvas( std::filesystem::path fontPath )
+        GuiCanvas()
+        {
+
+        }
+
+        void writeText( std::string text, sf::Font* fontPointer, uint size, sf::Text::Style style, sf::Vector2f position )
+        {
+            // Construct text object in vector
+            sf::Text& textRef = this->texts.emplace_back( text, *fontPointer, size );
+
+            // Update other properties not included in constructor
+            textRef.setStyle( style );
+            textRef.setPosition( position );
+        }
+
+        void writeText( std::string text, sf::Font* fontPointer, uint size )
+        {
+            // Construct text object in vector
+            sf::Text& textRef = this->texts.emplace_back( text, *fontPointer, size );
+        }
+
+        std::vector<sf::Text>& getTexts()
+        {
+            return this->texts;
+        }
+
+        private:
+            virtual void draw( sf::RenderTarget& target, sf::RenderStates states ) const
+            {
+                // apply the entity's transform -- combine it with the one that was passed by the caller
+                states.transform *= this->getTransform(); // getTransform() is defined by sf::Transformable
+
+                for ( auto& text : this->texts )
+                    target.draw( text );
+            }
+
+            std::vector<sf::Text> texts;
     };
 }
--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,6 @@
 #include <SFML/Graphics.hpp>
 #include <iostream>
+#include <string>
 
 #include "argspp.h"
 
@@ -7,6 +8,7 @@
 #include "map.hpp"
 #include "engine.hpp"
 #include "entity.hpp"
+#include "gui.hpp"
 #include "player.hpp"
 
 using namespace std;
@@ -15,24 +17,33 @@ int main( int argc, char** argv )
 {
     // Argument parsing
     args::ArgParser parser{};
-    parser.helptext = "Usage: <executable name> --asset-dir=\"\" ...";
+    parser.helptext = "Usage: <executable name> [--asset-dir=\"...\"] [--framerate <number>] ...";
     parser.version = "0.1";
-    parser.option( "asset-dir", "/workspace/silenced-engine/SilencedAssets/" );
-    parser.flag( "quiet q" );
+
+    parser.option( "asset-dir assetdir assets", "/workspace/silenced-engine/SilencedAssets/" );
+    parser.option( "framerate", "30" );
+    //parser.flag( "quiet q" );
     parser.parse( argc, argv );
+
     std::filesystem::path assetDirectoryPath{ parser.value( "asset-dir" ) };
+    uint framerateLimit = stoi( parser.value( "framerate" ) );
 
     // Global stuff
     Engine::AssetCache<sf::Texture> tilesetTextureCache{ assetDirectoryPath / "Graphics/Tilesets/" };
     Engine::AssetCache<sf::Texture> spriteAtlasTextureCache{ assetDirectoryPath / "Graphics/Characters/" };
+    Engine::AssetCache<sf::Font> fontCache{ assetDirectoryPath / "Fonts" };
 
     Engine::Player player{ spriteAtlasTextureCache.getAssetPointer( "00.png" ), 1 };
     Engine::Entity entity{ spriteAtlasTextureCache.getAssetPointer( "22.png" ), 2 };
 
     Engine::Map map{ "/workspace/silenced-engine/TestMap2.map", tilesetTextureCache.getAssetPointer( "00.png" ) };
+
+    Engine::GuiCanvas canvas{};
+    canvas.writeText( "Hi there!", fontCache.getAssetPointer( "VL-Gothic-Regular(FromThatSteamGuy).ttf" ), 30 );
     
     // Engine instance
     Engine::GameEngine engine{ "Engine", 400, 300 };
+    engine.getWindow().setFramerateLimit( framerateLimit );
 
 
     auto loop = [ & ] ( float deltaTime )
@@ -48,6 +59,7 @@ int main( int argc, char** argv )
         engine.getWindow().draw( map );
         engine.getWindow().draw( entity );
         engine.getWindow().draw( player );
+        engine.getWindow().draw( canvas );
         engine.getWindow().display();
     };
 
